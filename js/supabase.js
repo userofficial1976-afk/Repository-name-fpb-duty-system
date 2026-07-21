a9d2ee37f3c86d07143fe0ae0b05a7f6b80d487d
userofficial1976-afk <userofficial1976@gmail.com>
2026-07-21 09:12:25 +0800

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://nlwrrrwkjpktrifxeymw.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_0ymP9thOFXkuNYzvwa0gwQ_1mbxyFE6";
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
