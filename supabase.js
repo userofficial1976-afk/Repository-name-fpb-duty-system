01fa092e46d09853c31eec4a27964efa43316b4d
userofficial1976-afk <userofficial1976@gmail.com>
2026-07-21 12:33:04 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
-const SUPABASE_URL =
-"https://nlwrrrwkjpktrifxeymw.supabase.co";
+const SUPABASE_URL = "https://nlwrrrwkjpktrifxeymw.supabase.co";
 
-const SUPABASE_KEY =
-"sb_publishable_0ymP9thOFXkuNYzvwa0gwQ_1mbxyFE6";
+const SUPABASE_KEY = "sb_publishable_0ymP9thOFXkuNYzvwa0gwQ_1mbxyFE6";
 
-const supabaseClient =
-supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+console.log("SUPABASE BERJAYA DISAMBUNG");
